8e196f02f2ba36182f4eaa1a73d2ea329b65a999
Abhay Vatsa <atvatsa@gmail.com>
2021-06-19 20:09:23 -0700

    Wrapping tests in wrapInClient

--- src/__tests__/api.test.ts
+++ src/__tests__/api.test.ts
@@ -1,62 +1,56 @@
-import { createRpc } from './app';
+import { wrapInClient, Client } from './app';
 
 describe('API', () => {
-  it.concurrent('Test a synchronous method returning non-Result', async () => {
-    const rpc = await createRpc();
-    const result = await rpc.client.hello('world');
+  it.concurrent(
+    'Test a synchronous method returning non-Result',
+    wrapInClient(async ({ hello }: Client) => {
+      const result = await hello('world');
 
-    expect(result.ok).toEqual(true);
-    expect(result.val).toEqual('Hello world!!');
-
-    rpc.teardown();
-  });
+      expect(result.ok).toEqual(true);
+      expect(result.val).toEqual('Hello world!!');
+    })
+  );
 
   it.concurrent(
     'Test an asynchronous method returning non-Result',
-    async () => {
-      const rpc = await createRpc();
-      const { val } = await rpc.client.goodbye('abhay');
+    wrapInClient(async ({ goodbye }: Client) => {
+      const { val } = await goodbye('abhay');
 
       expect(val).toEqual('Goodbye abhay!');
-
-      rpc.teardown();
-    }
+    })
   );
 
-  it.concurrent('Test an asynchronous method return OkImpl', async () => {
-    const rpc = await createRpc();
-    const { ok, val } = await rpc.client.getUnixTime();
+  it.concurrent(
+    'Test an asynchronous method return OkImpl',
+    wrapInClient(async ({ getUnixTime }: Client) => {
+      const { ok, val } = await getUnixTime();
 
-    expect(ok && typeof val === 'number');
-
-    rpc.teardown();
-  });
+      expect(ok && typeof val === 'number');
+    })
+  );
 
   it.concurrent(
     'Test an asynchronous method returning OkImpl/ErrImpl',
-    async () => {
-      const rpc = await createRpc();
-      const result = await rpc.client.getPeople();
+    wrapInClient(async ({ getPeople }: Client) => {
+      const result = await getPeople();
 
       expect(
         (result.err && result.val === 'ahh!') ||
           (result.ok && result.val.length === 3)
       ).toEqual(true);
-
-      rpc.teardown();
-    }
+    })
   );
 
-  it.concurrent('Test an asynchronous method returning Result', async () => {
-    const rpc = await createRpc();
-    const result = await rpc.client.divide(10, 0);
+  it.concurrent(
+    'Test an asynchronous method returning Result',
+    wrapInClient(async ({ divide }: Client) => {
+      const result = await divide(10, 0);
 
-    expect(result.ok).toEqual(false);
-    expect(result.val === 'Divided by zero').toBeTruthy();
-    if (!result.ok) {
-      expect(result.stack).toBeTruthy();
-    }
-
-    rpc.teardown();
-  });
+      expect(result.ok).toEqual(false);
+      expect(result.val === 'Divided by zero').toBeTruthy();
+      if (!result.ok) {
+        expect(result.stack).toBeTruthy();
+      }
+    })
+  );
 });
