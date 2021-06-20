ad468d8b42d6c03d940e5caa41da3cf68d31aed8
Abhay Vatsa <atvatsa@gmail.com>
2021-06-20 00:59:21 -0700

    Request type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,29 @@
-import type * as express from 'express';
-import { NextApiRequest, NextApiResponse } from 'next';
+import * as http from 'http';
 
-export type Request = express.Request | NextApiRequest;
-export type Response = express.Response | NextApiResponse;
-export type NextFunction = express.NextFunction;
+export interface Request extends http.IncomingMessage {
+  query: {
+    [key: string]: string | string[];
+  };
+  cookies: {
+    [key: string]: string;
+  };
+  body: any;
+  method?: string;
+  url?: string;
+  statusCode?: number;
+  statusMessage?: string;
+}
+
+declare type Send<T> = (body: T) => void;
+
+export interface Response<T = any> extends http.ServerResponse {
+  // Send data `any` data in response
+  send: Send<T>;
+
+  // Send data `json` data in response
+  json: Send<T>;
+  redirect(url: string): Response<T> | any;
+  redirect(status: number, url: string): Response<T> | any;
+
+  status: (statusCode: number) => Response<T>;
+}
