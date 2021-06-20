2234838d7074a4fa2469d5bee037cf5c802dc082
Abhay Vatsa <atvatsa@gmail.com>
2021-06-19 20:09:23 -0700

    Wrapping tests in wrapInClient

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,42 +1,45 @@
-import { createRpc } from './app';
+import { createRpc, wrapInClient, Client } from './app';
 import fetch from 'cross-fetch';
 
 describe('server error handling', () => {
-  it.concurrent('Test a method that does not exist', async () => {
-    const rpc = await createRpc();
-    // @ts-expect-error: We are destructured a method that does not exist on client
-    const { DOES_NOT_EXIST } = rpc.client;
-    const { val, ok, stack } = await DOES_NOT_EXIST('world');
+  it.concurrent(
+    'Test a method that does not exist',
+    wrapInClient(async (client: Client) => {
+      // @ts-expect-error: We are destructured a method that does not exist on client
+      const { DOES_NOT_EXIST } = client;
+      const { val, ok, stack } = await DOES_NOT_EXIST('world');
 
-    expect(ok).toEqual(false);
-    expect(stack).toBeTruthy();
-    expect(val.includes('Method does not exist')).toBeTruthy();
-
-    rpc.teardown();
-  });
+      expect(ok).toEqual(false);
+      expect(stack).toBeTruthy();
+      expect(val.includes('Method does not exist')).toBeTruthy();
+    })
+  );
 
   it.concurrent('Try a method other than HTTP POST', async () => {
     const rpc = await createRpc();
-    const { result } = await fetch(rpc.rpcUrl, {
-      method: 'DELETE',
-      body: JSON.stringify({ jsonrpc: '2.0' }),
-      headers: { 'Content-Type': 'application/json' },
-    }).then((r) => r.json());
 
-    expect(
-      result.val.includes('Invariant failed: Only HTTP POST is supported')
-    ).toBe(true);
+    try {
+      const { result } = await fetch(rpc.rpcUrl, {
+        method: 'DELETE',
+        body: JSON.stringify({ jsonrpc: '2.0' }),
+        headers: { 'Content-Type': 'application/json' },
+      }).then((r) => r.json());
 
-    rpc.teardown();
+      expect(
+        result.val.includes('Invariant failed: Only HTTP POST is supported')
+      ).toBe(true);
+    } finally {
+      rpc.teardown();
+    }
   });
 
-  it.concurrent('Try a method prefixed with _', async () => {
-    const rpc = await createRpc();
-    // This is usually a private method to the server
-    const result = await rpc.client._superSecretMethod();
+  it.concurrent(
+    'Try a method prefixed with _',
+    wrapInClient(async ({ _superSecretMethod }: Client) => {
+      // This is usually a private method to the server
+      const result = await _superSecretMethod();
 
-    expect(result.err).toEqual(true);
-
-    rpc.teardown();
-  });
+      expect(result.err).toEqual(true);
+    })
+  );
 });
--- src/__tests__/typechecking.test.ts
+++ src/__tests__/typechecking.test.ts
@@ -1,18 +1,18 @@
 import { assert, IsExact } from 'conditional-type-checks';
 import { RpcError } from '../';
-import { createRpc } from './app';
+import { wrapInClient, Client } from './app';
 
 describe('typechecking', () => {
-  it.concurrent('type narrowing works correctly', async () => {
-    const rpc = await createRpc();
-    const result = await rpc.client.divide(10, 0);
+  it.concurrent(
+    'type narrowing works correctly',
+    wrapInClient(async ({ divide }: Client) => {
+      const result = await divide(10, 0);
 
-    if (result.ok) {
-      assert<IsExact<typeof result.val, number>>(true);
-    } else {
-      assert<IsExact<typeof result.val, 'Divided by zero' | RpcError>>(true);
-    }
-
-    rpc.teardown();
-  });
+      if (result.ok) {
+        assert<IsExact<typeof result.val, number>>(true);
+      } else {
+        assert<IsExact<typeof result.val, 'Divided by zero' | RpcError>>(true);
+      }
+    })
+  );
 });
