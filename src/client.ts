6872ef27c168545c11a9e53f2e9c0c89ca20a559
Abhay Vatsa <atvatsa@gmail.com>
2021-06-26 18:24:53 -0700

    no worky

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
 import { Ok, Err, Result, ResultOkType, ResultErrType } from './result';
 import invariant from 'tiny-invariant';
-import { Request } from './server';
+import { Methods, Request, Services } from './server';
 
 export type Fetch = typeof window.fetch;
 
@@ -34,13 +34,80 @@ export type Client<T> = {
     : never;
 };
 
+//type ValueOf<T> = T[keyof T];
+//type Abhay<T, K> = T extends Methods ? Client<T> : never;
+//type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
+
+type Proxify<T> = {
+  [K in keyof T]: T[K] extends Methods ? Client<T[K]> : never;
+};
+
 /**
  * The Client uses a typed proxy to make API calls to the server.
  */
-export function createClient<T>(url: string, fetch = window?.fetch): Client<T> {
+export function createClient<T>(
+  services: T,
+  url: string,
+  fetch = window?.fetch
+) {
   invariant(typeof fetch !== 'undefined', 'not passed a valid `fetch`');
+  const keys = Object.keys(services) as Array<keyof T>;
 
+  const acc = {} as Proxify<T>;
+  //const acc = {} as T;
   let idSeed = 1;
+
+  const proxies = keys.reduce((service) => {
+    acc[service] = new Proxy(
+      {},
+      {
+        get(_, method: string) {
+          if (method === 'then') return null; // Proxy is not thennable (enable returning from async functions)
+
+          return async (...params: unknown[]) => {
+            const id = (idSeed++).toString();
+
+            try {
+              const response = await fetch(url, {
+                method: 'post',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  jsonrpc: '2.0',
+                  service,
+                  method,
+                  params,
+                  id,
+                }),
+              });
+              const text = await response.text();
+              const json = JSON.parse(text);
+
+              invariant(json.jsonrpc === '2.0', 'invalid jsonrpc version');
+              invariant(json.id === id, 'invalid response id');
+
+              const {
+                result: { err, val, _stack },
+              } = json;
+
+              if (err) {
+                const e = Err(val);
+                (e as any)._stack = _stack;
+                return e;
+              }
+
+              return Ok(val);
+            } catch (err) {
+              return Err(new RpcError('Unexpected request error!'));
+            }
+          };
+        },
+      }
+    ) as Client<T[typeof service]>;
+  });
+
+  return proxies;
 
   return new Proxy(
     {},
