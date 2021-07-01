0ead67ba2aab8312d3bf51b3c4295a4d13dbfac2
Abhay Vatsa <atvatsa@gmail.com>
2021-06-30 18:35:38 -0700

    Adding some error checks

--- src/__tests__/api.test.ts
+++ src/__tests__/api.test.ts
@@ -34,20 +34,36 @@ describe('Test a range of return values', () => {
   it.concurrent(
     'Asynchronous method returning Ok()/Err() works',
     wrapInClient(async ({ math }) => {
-      const result = await math.multiply(10, 2);
+      {
+        const result = await math.multiply(10, 2);
 
-      expect(result.ok).toEqual(true);
-      expect(result.val).toEqual(10 * 2);
+        expect(result.ok).toEqual(true);
+        expect(result.val).toEqual(10 * 2);
+      }
+      {
+        const result = await math.multiply(Number.MAX_VALUE, Number.MAX_VALUE);
+
+        expect(result.ok).toEqual(false);
+        expect(result.val).toEqual('Number overflow');
+      }
     })
   );
 
   it.concurrent(
     'Asynchronous method returning manually annotated Result works',
     wrapInClient(async ({ math }) => {
-      const result = await math.divide(10, 2);
+      {
+        const result = await math.divide(10, 2);
 
-      expect(result.ok).toEqual(true);
-      expect(result.val).toEqual(10 / 2);
+        expect(result.ok).toEqual(true);
+        expect(result.val).toEqual(10 / 2);
+      }
+      {
+        const result = await math.divide(10, 0);
+
+        expect(result.ok).toEqual(false);
+        expect(result.val === 'Divided by zero');
+      }
     })
   );
 });
