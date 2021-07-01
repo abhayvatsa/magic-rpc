98aa3d78b06db468648ef77eec5b160860cc9b59
Abhay Vatsa <atvatsa@gmail.com>
2021-06-30 19:10:01 -0700

    Cleaning up types

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
 import { Ok, Err, Result, ResultOkType, ResultErrType } from './result';
 import invariant from 'tiny-invariant';
-import { Methods, Request, Services } from './server';
+import { Request } from './server';
 
 export type Fetch = typeof window.fetch;
 
@@ -21,7 +21,7 @@ declare type ExcludeRequest<T> = T extends [_: Request, ...args: infer A]
   ? A
   : T;
 
-export type Client<T> = {
+type ClientService<T> = {
   [K in keyof T]: T[K] extends (...args: infer A) => infer P
     ? (
         ...args: ExcludeRequest<A>
@@ -34,13 +34,8 @@ export type Client<T> = {
     : never;
 };
 
-//type ValueOf<T> = T[keyof T];
-//type Abhay<T, K> = T extends Methods ? Client<T> : never;
-//type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
-
-type Proxify<T> = {
-  //[K in keyof T]: T[K] extends Methods ? Client<T[K]> : never;
-  [K in keyof T]: Client<T[K]>; //T[K] extends Methods ? string : never;
+export type Client<T> = {
+  [K in keyof T]: ClientService<T[K]>;
 };
 
 /**
@@ -54,10 +49,10 @@ export function createClient<T>(
   invariant(typeof fetch !== 'undefined', 'not passed a valid `fetch`');
   const keys = Object.keys(services) as Array<keyof T>;
 
-  const acc = {} as Proxify<T>;
+  const acc = {} as Client<T>;
   let idSeed = 1;
 
-  const proxies = keys.reduce((acc, service) => {
+  return keys.reduce((acc, service) => {
     acc[service] = new Proxy(
       {},
       {
@@ -102,56 +97,7 @@ export function createClient<T>(
           };
         },
       }
-    ) as Client<typeof services[typeof service]>;
+    ) as ClientService<typeof services[typeof service]>;
     return acc;
   }, acc);
-
-  return proxies;
-
-  return new Proxy(
-    {},
-    {
-      get(_, method: string) {
-        if (method === 'then') return null; // Proxy is not thennable (enable returning from async functions)
-
-        return async (...params: unknown[]) => {
-          const id = (idSeed++).toString();
-
-          try {
-            const response = await fetch(url, {
-              method: 'post',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                jsonrpc: '2.0',
-                method,
-                params,
-                id,
-              }),
-            });
-            const text = await response.text();
-            const json = JSON.parse(text);
-
-            invariant(json.jsonrpc === '2.0', 'invalid jsonrpc version');
-            invariant(json.id === id, 'invalid response id');
-
-            const {
-              result: { err, val, _stack },
-            } = json;
-
-            if (err) {
-              const e = Err(val);
-              (e as any)._stack = _stack;
-              return e;
-            }
-
-            return Ok(val);
-          } catch (err) {
-            return Err(new RpcError('Unexpected request error!'));
-          }
-        };
-      },
-    }
-  ) as Client<T>;
 }
